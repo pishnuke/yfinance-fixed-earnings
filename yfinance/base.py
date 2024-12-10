9871e710266b73095b101c54e9577337c03520fe
Evgeny Pishnyuk <pishnuke@gmail.com>
2024-12-09 22:54:38 -0500

    Update base.py

--- yfinance/base.py
+++ yfinance/base.py
@@ -683,7 +683,8 @@ class TickerBase:
         self._quote.proxy = proxy or self.proxy
         
         tz_backup = self._get_ticker_tz(proxy=proxy, timeout=30)
-        dates[cn] = dates[cn].dt.tz_localize(tz_backup)
+        print(tz_backup)
+        # dates[cn] = dates[cn].dt.tz_localize(tz_backup)
 
         # if len(tzinfo['TZ'].unique())==1:
         #     try:
